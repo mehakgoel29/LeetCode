04d7bdd771dcded0b00f23da2e17a5700974a41b
Mehak Goel <mhkgoel29@gmail.com>
2024-12-22 15:09:00 +0530

    problem

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,6 +1,14 @@
 public class MergeSortedArray {
     class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+           //If you iterate from the beginning (index 0) of nums1 and nums2,
+           // you'll run into a problem: the elements of nums1 will be overwritten before their comparison is complete. 
+           //This happens because the merging process modifies nums1 in place, and starting from the beginning would
+           // destroy the original order of nums1.
+
+
+           
            int i = m - 1; // Pointer for the last element in the first m elements of nums1
             int j = n - 1; // Pointer for the last element in nums2
             int k = m + n - 1; // Pointer for the last position in nums1
