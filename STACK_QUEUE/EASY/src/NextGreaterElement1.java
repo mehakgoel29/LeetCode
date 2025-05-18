a6d5fd58ab806c76861ecdbc4fe47d17e175f543
Mehak Goel <mhkgoel29@gmail.com>
2025-05-18 15:13:52 +0530

    question on stack

--- STACK_QUEUE/EASY/src/NextGreaterElement1.java
+++ STACK_QUEUE/EASY/src/NextGreaterElement1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Stack;
 
@@ -56,16 +57,19 @@ public class NextGreaterElement1 {
             Stack<Integer> stack = new Stack<>(); // O(m)
 
             // Step 1: Build map of next greater elements for nums2
-            for (int i = 0; i < nums2.length; i++) {
+            for (int i = nums2.length - 1; i >= 0; i--) {
                 int num = nums2[i];
-                // checking !stack.isEmpty() so that checking peek do not throw exception
-                while (!stack.isEmpty() && num > stack.peek()) {
-                    int smaller = stack.pop();
-                    map.put(smaller, num); // Map each popped element to its next greater
+                while (!stack.isEmpty() && stack.peek() <= num) {
+                    stack.pop();
+                }
+                // If stack is not empty, then the top is the next greater element
+                if (!stack.isEmpty()) {
+                    map.put(num, stack.peek());
+                } else {
+                    map.put(num, -1); // Or any default value indicating no greater element
                 }
                 stack.push(num);
             }
-
             // Step 2: For remaining elements in stack, no greater element exists
             while (!stack.isEmpty()) {
                 map.put(stack.pop(), -1);
@@ -79,4 +83,32 @@ public class NextGreaterElement1 {
 
             return res;
         }
+
+
+
+        // when only one array is given
+        public int[] nextGreaterElementOptimal(int[] arr) {
+        Stack<Integer> s= new Stack<>();
+        int[] res=new int[arr.length];
+            for (int i = arr.length-1; i >=0 ; i--) {
+                int num = arr[i];
+                while (!s.isEmpty() && s.peek() <= num) {
+                    s.pop();
+                }
+                if (s.isEmpty()) {
+                    res[i] = -1;
+                } else {
+                    res[i] = s.peek();
+                }
+                s.push(num);
+            }
+            return res;
+}
+    public static void main(String[] args) {
+        NextGreaterElement1 ng = new NextGreaterElement1();
+        int[] arr = {4, 5, 2, 25};
+        int[] result = ng.nextGreaterElementOptimal(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
 }
